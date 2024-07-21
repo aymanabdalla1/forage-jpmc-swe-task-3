b3b9907d2a5c6a31031c7f4ddcf2d1da67229c59
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-07-21 17:07:29 -0500

    updated graph to show upper/lower bounds and triggers for stock ratio

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,33 @@
 import { ServerRespond } from './DataStreamer';
 
-export interface Row {
-  stock: string,
-  top_ask_price: number,
+export interface Row {  //updating the interface to include the upper_bound, lowerbound and trigger_alert
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lowerbound: number,
+  trigger_alert: number | undefined
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2; //calculating the average price of stock ABC
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2; //calculating the average price of stock DEF
+    const ratio = priceABC / priceDEF; //calculating the ratio of the price of stock ABC to stock DEF
+    const upperBound = 1 + 0.05; //setting the upper bound to 5% above the ratio
+    const lowerBound = 1 - 0.05; //setting the lower bound to 5% below the ratio
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+                 serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined, //setting the trigger alert to the ratio if it crosses the upper or lower bound
+    };
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
+import { time } from 'console';
 
 interface IProps {
   data: ServerRespond[],
@@ -23,10 +24,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      price_abc: 'float', // The price of stock ABC.
+      price_def: 'float', // The price of stock DEF.
+      ratio: 'float', // The ratio of the price of stock ABC to stock DEF.
+      timestamp: 'date', // The time when the data was received.
+      upper_bound: 'float', // The upper bound of the ratio.
+      lowerbound: 'float', // The lower bound of the ratio.
+      trigger_alert: 'float', // The trigger alert of when bound is crossed.
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +39,25 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+      elem.setAttribute('view', 'y_line'); // Set the default view to line chart.
+      elem.setAttribute('row-pivots', '["timestamp"]'); // Set the default row pivot to timestamp.
+      elem.setAttribute('columns', '["ratio", "lowerbound", "upper_bound", "trigger_alert"]'); // Set the default columns to display.
+      elem.setAttribute('aggregates', JSON.stringify({ // Set the default aggregate values.
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lowerbound: 'avg',
+        trigger_alert: 'avg'
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),] as unknown as TableData);
     }
   }
 }
